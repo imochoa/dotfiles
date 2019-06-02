080dd0684fbc170a06e7b86be3b54588b45f5dc4
ignacio <nachomartinochoa@gmail.com>
2019-06-02 22:18:57 +0200

    added the git clone starter for the hosts config file

---
+++ scripts/configure-hosts.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# TODO Block trackers etc.
+# git clone https://github.com/StevenBlack/hosts.git
