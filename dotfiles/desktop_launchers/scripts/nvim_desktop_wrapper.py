e15bd89d80c6e5e3784c1eb0f729fe92d57c343a
imochoa <nachomartinochoa@gmail.com>
2020-11-06 15:56:17 +0100

    - improved the neovim desktop wrapper

--- dotfiles/desktop_launchers/scripts/nvim_desktop_wrapper.py
+++ dotfiles/desktop_launchers/scripts/nvim_desktop_wrapper.py
@@ -1,23 +1,105 @@
-#!/usr/bin/env python3
+#! /usr/bin/python3.8
+"""Run nvim inside gnome-terminal"""
 
-# https://gist.github.com/fmoralesc/bd46e97b39ee284af641
-
-from sys import argv
-from subprocess import Popen
-from time import time
+import sys
+import os
+import subprocess
 import dbus
-
-session_bus = dbus.SessionBus()
-
-# Use the default term? update-alternatives --list x-terminal-emulator | head -n1
+from time import time
 
 
-# launch the terminal server with a custom app-id and window class (so the .desktop file gets associated)
-Popen("/usr/lib/gnome-terminal/gnome-terminal-server --app-id org.neovim --class=neovim".split())
-# wait until the name is registered
-timeout = time() + 1  # [s]
-while not session_bus.name_has_owner('org.neovim') and time() <= timeout:
-    pass
-# launch nvim in a gnome-terminal instance
-if session_bus.name_has_owner('org.neovim'):
-    Popen("gnome-terminal --app-id org.neovim -- nvim".split() + argv[1:])
+APP_ID = 'org.neovim'
+CLASS = 'neovim'
+NAME = 'Neovim'
+
+def find_terminal_server():
+    candidates = list(filter(os.path.exists, \
+                            ('/usr/lib/gnome-terminal/gnome-terminal-server', # arch et al
+                             '/usr/lib/gnome-terminal-server',
+                             '/usr/libexec/gnome-terminal-server')))          # fedora 22
+    if len(candidates) > 0:
+        return candidates[0]
+    else:
+        print("nvim-wrapper needs gnome-terminal-server, but it wasn't found.")
+        sys.exit()
+
+SERVER_CMD = [
+    find_terminal_server(),
+    '--app-id', APP_ID,
+    '--class', CLASS,
+    '--name', NAME,
+]
+TERM_CMD = [
+    'gnome-terminal',
+    '--app-id', APP_ID,
+    '--class', CLASS,
+    '--name', NAME,
+    '--hide-menubar',
+    '-x',
+    'nvim',
+]
+GTERM_PASSTHROUGH_OPTIONS = [
+    '--full-screen',
+    '--maximize',
+    '--profile',
+    '--geometry',
+    '--working-directory',
+    '--display'
+]
+
+def processArgv():
+    argv = sys.argv[1:]
+    gtermOptions = []
+    nvimOptions = []
+
+    for arg in argv:
+        argParts = arg.split('=', 1)
+        if argParts[0] in GTERM_PASSTHROUGH_OPTIONS:
+            gtermOptions.append(arg)
+        elif arg=='-f':
+            gtermOptions.append('--wait')
+        else:
+            nvimOptions.append(arg)
+
+    # launch the wrapper in the current directory by default
+    if not any([o.startswith('--working-directory') for o in gtermOptions]):
+        gtermOptions.append('--working-directory='+os.path.abspath(os.curdir))
+
+    # enable title by default
+    nvimOptions.append("+set title")
+
+    # enable 24-bit color
+    nvimOptions.append("+set termguicolors")
+
+    return {
+        'gterm': gtermOptions,
+        'nvim': nvimOptions
+    }
+
+def main():
+    """Run nvim inside gnome-terminal"""
+    session_bus = dbus.SessionBus()
+
+    # launch the terminal server with a custom app-id
+    # and window class (so the .desktop file gets associated)
+    if not session_bus.name_has_owner(APP_ID):
+        subprocess.Popen(SERVER_CMD)
+
+    # wait until the name is registered, or 2 seconds pass (when launching from
+    # cold cache it might take more time)
+    timeout = time() + 2
+    while not session_bus.name_has_owner(APP_ID) and time() <= timeout:
+        pass
+    # launch nvim in a gnome-terminal instance
+    if session_bus.name_has_owner(APP_ID):
+        options = processArgv()
+        cmd = [] + TERM_CMD[:-2] + options['gterm'] + TERM_CMD[-2:] + options['nvim']
+        with open(os.devnull, 'wb') as fnull:
+            p=subprocess.Popen(cmd,
+                             stdout=fnull,
+                             stderr=fnull)
+        if '--wait' in options['gterm']:
+            p.wait()
+
+if __name__ == '__main__':
+    main()
