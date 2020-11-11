bf6fbc8fe26b959a01bbf9021c3a1ec6c7b41679
imochoa <nachomartinochoa@gmail.com>
2020-11-11 12:28:33 +0100

    - "$@" is more stable than $@

--- bin/install_pkgs.sh
+++ bin/install_pkgs.sh
@@ -3,4 +3,4 @@
 # Wrapper so that you don't have to worry about the PYTHONPATH while calling the scripts
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
 REPODIR=$(realpath "${BINDIR}/..");
-PYTHONPATH="${REPODIR}" python3 ${REPODIR}/jumpstart/scripts/installer.py ${@}
+PYTHONPATH="${REPODIR}" python3 ${REPODIR}/jumpstart/scripts/installer.py "$@"
