5985b009d19464464ab2e1e4858cea76cfd68a0a
imochoa <nachomartinochoa@gmail.com>
2021-03-23 16:04:50 +0100

    - improved the update-loose-files.sh script

--- bin/update-loose-files.sh
+++ bin/update-loose-files.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # SETUP
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
 REPODIR=$(realpath "${BINDIR}/..");
 DOTFILEDIR="${REPODIR}/dotfiles";
 
-echo "Cheat autocompletions file...";
-wget https://raw.githubusercontent.com/cheat/cheat/master/scripts/cheat.bash -O "${DOTFILEDIR}/bash/completions/cheat.bash" || notify-send -u "critical"  "FAILED" && true;
+
+FILE="$(find ${DOTFILEDIR} -type f -iname 'cheat.bash')";
+usr/bin/wget https://raw.githubusercontent.com/cheat/cheat/master/scripts/cheat.bash \
+-O "${FILE}" || notify-send -u "critical"  "Cheat autocompletion FAILED" && true;
+
+
+FILE="$(find ${DOTFILEDIR} -type f -iname 'nvim_desktop_wrapper.py')";
+wget https://raw.githubusercontent.com/fmoralesc/neovim-gnome-terminal-wrapper/master/nvim-wrapper \
+-O "${FILE}" || notify-send -u "critical"  "neovim wrapper FAILED" && true;
--- dotfiles/desktop_launchers/scripts/nvim_desktop_wrapper.py
+++ dotfiles/desktop_launchers/scripts/nvim_desktop_wrapper.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+
 """Run nvim inside gnome-terminal"""
 
 import sys
